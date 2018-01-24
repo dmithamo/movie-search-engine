893da46f6ee08d1b3f4b25f40fc422def9d3ae1f
dbdennot8 <dennisbmithamo@gmail.com>
2018-01-24 19:41:39 +0300

    Set up and test the request module

--- app.js
+++ app.js
@@ -3,13 +3,33 @@
 var express = require("express");
 var app = express();
 
+// Import request module
+var request = require("request");
+
+
+// Set up global variable 'movies' to be reassigned as list of retrieved movies
+var movies;
+
+// Make request to OMDB API
+query = "iron man"
+var omdbUrl = "http://www.omdbapi.com/?&apikey=b9a1e8c8&y=2013&s=" + query
+request(omdbUrl, function(error, response, body){
+    if(!error && response.statusCode === 200){
+        var jsonData = JSON.parse(body)
+        movies = jsonData["Search"]
+    }else{
+        console.log("REPORT\n" + "--".repeat(20) + "\n" + "error: " + error + "statusCode: "+ response.statusCode)
+    }
+})
+
+
 // Set default render engine as ejs
 app.set("view engine", "ejs");
 
 // The root route. Render Homepage
 app.get("/", function(req, res){
     var title = "T-Nane's Movie Search Engine";
-    res.render("homepage", {title:title});
+    res.render("homepage", {title:title, movies:movies});
 })
 
 
